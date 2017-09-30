3d110d5187f5ab530bde59b61746f3094abf19bc
Javier Novoa C <jstitch@gmail.com>
2017-09-30 12:19:22 -0500

    Servo forward, backward, turning maneauvers script

---
+++ Servo/ForwardLeftRightBackward/ForwardLeftRightBackward.ino
@@ -0,0 +1,43 @@
+// Robotics with the BOE Shield - ForwardLeftRightBackward
+// Move forward, left, right, then backward for testing and tuning.
+
+#include <Servo.h>                           // Include servo library
+
+Servo servoLeft;                             // Declare left and right servos
+Servo servoRight;
+ 
+void setup()                                 // Built-in initialization block
+{ 
+  tone(4, 3000, 1000);                       // Play tone for 1 second
+  delay(1000);                               // Delay to finish tone
+
+  servoLeft.attach(13);                      // Attach left signal to P13 
+  servoRight.attach(12);                     // Attach right signal to P12
+
+  // Full speed forward
+  servoLeft.writeMicroseconds(1593);         // Left wheel counterclockwise
+  servoRight.writeMicroseconds(1300);        // Right wheel clockwise
+  delay(2000);                               // ...for 2 seconds
+
+  // Turn left in place
+  servoLeft.writeMicroseconds(1300);         // Left wheel clockwise
+  servoRight.writeMicroseconds(1300);        // Right wheel clockwise
+  delay(795);                                // fullcharged battery: ... 90°
+
+  // Turn right in place
+  servoLeft.writeMicroseconds(1700);         // Left wheel counterclockwise
+  servoRight.writeMicroseconds(1700);        // Right wheel counterclockwise
+  delay(798);                                // fullcharged battery: ... 90°
+
+  // Full speed backward
+  servoLeft.writeMicroseconds(1396);         // Left wheel clockwise
+  servoRight.writeMicroseconds(1700);        // Right wheel counterclockwise
+  delay(2000);                               // ...for 2 seconds
+
+  servoLeft.detach();                        // Stop sending servo signals
+  servoRight.detach(); 
+}  
+
+void loop()                                  // Main loop auto-repeats
+{                                            // Empty, nothing needs repeating
+} 
