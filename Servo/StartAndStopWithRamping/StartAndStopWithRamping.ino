89060a124e98bef58c04d9041065953caace493b
Javier Novoa C <jstitch@gmail.com>
2017-10-06 21:16:30 -0500

    Forward and Backward with Ramping

--- Servo/StartAndStopWithRamping/StartAndStopWithRamping.ino
+++ Servo/StartAndStopWithRamping/StartAndStopWithRamping.ino
@@ -5,7 +5,7 @@
  
 Servo servoLeft;                             // Declare left and right servos
 Servo servoRight;
- 
+
 void setup()                                 // Built-in initialization block
 { 
   tone(4, 3000, 1000);                       // Play tone for 1 second
@@ -13,23 +13,25 @@ void setup()                                 // Built-in initialization block
 
   servoLeft.attach(13);                      // Attach left signal to P13 
   servoRight.attach(12);                     // Attach right signal to P12
-  
+
+  // Forward
   for(int speed = 0; speed <= 100; speed += 2) // Ramp up to full speed.
   {
     servoLeft.writeMicroseconds(1500+speed);   // us = 1500,1502,...1598,1600
     servoRight.writeMicroseconds(1500-speed);  // us = 1500,1498,...1402,1400
     delay(20);                                 // 20 ms at each speed
-  } 
+  }
    
   delay(1500);                                 // Full speed for 1.5 seconds
 
-  for(int speed = 100; speed >= 0; speed -= 2) // Ramp from full speed to stop 
+  for(int speed = 100; speed >= 0; speed -= 2) // Ramp from full speed to stop
   {
     servoLeft.writeMicroseconds(1500+speed);   // us = 1600,1598,...1502,1500
     servoRight.writeMicroseconds(1500-speed);  // us = 1400,1402,...1498,1500
     delay(20);                                 // 20 ms at each speed
-  } 
-  
+  }
+
+
   servoLeft.detach();                          // Stop sending servo signals
   servoRight.detach(); 
 }
@@ -37,4 +39,3 @@ void setup()                                 // Built-in initialization block
 void loop()                                    // Main loop auto-repeats
 {                                              // Empty, nothing to repeat
 }
-
